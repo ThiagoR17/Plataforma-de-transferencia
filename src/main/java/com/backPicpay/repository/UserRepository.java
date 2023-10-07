7b9ca1f0637e5f719fa48871ce7c2475f3377fe0
ThiDev17 <thidev.17@gmail.com>
2023-10-07 11:20:51 -0300

    feat: create UserRepo

---
+++ src/main/java/com/backPicpay/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.backPicpay.repository;
+
+import com.backPicpay.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+   Optional<User> findUserByDocument(String document);
+}
+
+
