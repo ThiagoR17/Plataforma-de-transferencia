6d84b43ece3694a4d0346db44c0cba2d145b6d30
ThiDev17 <thidev.17@gmail.com>
2023-10-07 11:19:06 -0300

    feat: initial config

---
+++ src/main/java/com/backPicpay/BackPicpayApplication.java
@@ -0,0 +1,13 @@
+package com.backPicpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackPicpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackPicpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backPicpay/BackPicpayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backPicpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackPicpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
